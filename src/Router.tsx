6d2557e744cf4fd695062bb21d07d3cfb3b71e50
luizctb <luizcarlostb@hotmail.com>
2023-03-07 23:38:47 -0300

    router commit

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import { History } from './pages/History'
+import { History } from './pages/History';
 
 import { Home } from './pages/Home'
 import { DefaultLayout } from './layouts/DefaultLayout';
